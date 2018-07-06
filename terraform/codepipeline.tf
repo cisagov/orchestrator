e8fddb5e8191d111047dea23351ee15af620aa3f
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-07-06 11:12:14 -0400

    Adding default encryption to pipeline bucket

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -4,6 +4,14 @@ resource "aws_s3_bucket" "pipeline_bucket" {
   acl = "private"
   force_destroy = "true"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   # Destroy objects after 30 days
   lifecycle_rule {
     enabled = true
